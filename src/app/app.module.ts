5d54f1400b3b04696d31df9e56494d54a92a8872
Philipp <54907080+ErnstStavroBlofeld@users.noreply.github.com>
2020-07-02 23:19:25 +0200

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LinkComponent } from './link/link.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LinkComponent
+    LinkComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
