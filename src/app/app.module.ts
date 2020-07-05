881b30e8d06072d679151e230be44d123582adb0
Philipp <54907080+ErnstStavroBlofeld@users.noreply.github.com>
2020-07-05 18:17:53 +0200

    Constructed contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { BannerComponent } from './banner/banner.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ContactComponent } from './pages/contact/contact.component';
     AboutComponent,
     NotFoundComponent,
     BannerComponent,
-    ContactComponent
+    ContactComponent,
+    BusinessCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/business-card/business-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business-card',
+  templateUrl: './business-card.component.html',
+  styleUrls: ['./business-card.component.css']
+})
+export class BusinessCardComponent { }
