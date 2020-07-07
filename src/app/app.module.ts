c887f660ca8e1a739546242e59cb447898347786
Philipp <54907080+ErnstStavroBlofeld@users.noreply.github.com>
2020-07-08 01:28:46 +0200

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,7 @@ import { LinkComponent } from './link/link.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { BannerComponent } from './banner/banner.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { BusinessCardComponent } from './business-card/business-card.component';
-import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +17,9 @@ import { SectionComponent } from './section/section.component';
     HeaderComponent,
     LinkComponent,
     HomeComponent,
-    AboutComponent,
-    NotFoundComponent,
-    BannerComponent,
     ContactComponent,
-    BusinessCardComponent,
-    SectionComponent
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
