86b4ed581f7ae885b32b080273567886de1c48a7
Philipp <54907080+ErnstStavroBlofeld@users.noreply.github.com>
2020-07-07 22:39:16 +0200

    Added new fonts, changed app baner to svg

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,4 +1,4 @@
-import { animate, query, style, transition, trigger, stagger } from '@angular/animations';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 
 const Optional = {
   optional: true
@@ -15,23 +15,11 @@ export const RouteAnimation = trigger('routeAnimation', [
         transform: 'scale(0) translateY(100%)'
       })
     ], Optional),
-    query('app-section', [
-      style({
-        opacity: 0
-      })
-    ], Optional),
     query(':enter', [
       animate(
         '500ms ease',
         style({opacity: 1, transform: 'scale(1) translateY(0)'})
       )
-    ], Optional),
-    query('app-section', [
-      stagger('300ms', [
-        animate('200ms', style({
-          opacity: 1
-        }))
-      ])
     ], Optional),
   ])
 ]);
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,12 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
-  @Input() first: string;
-  @Input() second: string;
-  @Input() third: string;
-}
+export class BannerComponent { }
