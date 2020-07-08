ef34eecc59112e1fe65aa7fc1e8c1955b1573956
Philipp <54907080+ErnstStavroBlofeld@users.noreply.github.com>
2020-07-08 02:35:43 +0200

    Added simple navigation bar

--- src/app/link/link.component.ts
+++ src/app/link/link.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-link',
@@ -6,4 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./link.component.scss']
 })
 export class LinkComponent {
+  @Input() to: string;
 }
